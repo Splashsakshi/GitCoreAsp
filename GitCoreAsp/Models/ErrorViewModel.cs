23e5a5b03268d273278025dff1db40337d178110
portfolio <craftsharma08@gmail.com>
2024-11-26 16:49:08 -0800

    Add project files.

---
+++ GitCoreAsp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitCoreAsp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
